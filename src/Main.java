cf5add5b0f93c08af51a9743ff533a8e08232783
Kostya <snaaack@.mail.ru>
2022-06-12 03:31:37 +0200

    add note

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        double ticketPrice;
-        int priceOfOneMile;
-        int bonusMiles;
+        double ticketPrice; // Цена билета с копейками
+        int priceOfOneMile; // Стоимость одной бонусной мили
+        int bonusMiles; // Количество полученных бонусных миль
 
         ticketPrice = 529.99;
         priceOfOneMile = 20;
 
-        int ticketPriceInRub = (int) ticketPrice;
+        int ticketPriceInRub = (int) ticketPrice; // Цена билета в рублях без копеек
 
         bonusMiles = ticketPriceInRub / priceOfOneMile;
 
